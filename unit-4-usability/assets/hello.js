2edebd2fe2c3783bafc7982bf636b4c1ea59bcdb
kimberkonen <kkonen@mail.umw.edu>
2026-04-07 10:10:26 -0400

    unit 4

--- unit-4-usability/assets/hello.js
+++ unit-4-usability/assets/hello.js
@@ -1,15 +1,21 @@
 let myForm= document.querySelector("#myForm");
 
-myForm.addEventListener("submit", function (event) => {
-	event.preventDefault ();
+myForm.addEventListener("submit", (event) => {
+	event.preventDefault();
 	
+  let q1= Number (this.q1.value);
+  let q2= Number (this.q2.value);
+  let q3= Number (this.q3.value);
+  let q4= Number (this.q4.value);
+  let q5= Number (this.q5.value);
+
+  console.log(q1);
 	
-let inputs = document.querySelectorAll(".score");
+  let answer = (q1 + q2 + q3 + q4+ q5);
+this.answer.value= answer;
+console.log (answer);
 
-  let total = 0;
 
-
-	console.log("total score", total)
 
 });
 
