e99939851f5201e8b53cdeaeab174d8cea336ff4
kimberkonen <kkonen@mail.umw.edu>
2026-04-07 10:34:00 -0400

    unit 4

--- unit-4-usability/assets/hello.js
+++ unit-4-usability/assets/hello.js
@@ -9,11 +9,26 @@ myForm.addEventListener("submit", (event) => {
   let q4= Number (this.q4.value);
   let q5= Number (this.q5.value);
 
-  console.log(q1);
+//odd items: subtract 1 from user response
+
+  q1 -= 1;
+  q3 -= 1;
+  q5 -= 1;
+
+//even items; subtract response from 5
+
+  q2 = 5 - q2;
+  q4 = 5 - q4;
+
+//add converted responses
 	
-  let answer = (q1 + q2 + q3 + q4+ q5);
-this.answer.value= answer;
+  let answer = (q1 + q2 + q3 + q4+ q5) * 2.5;
+
+
 console.log (answer);
+
+this.answer.value = answer;
+
 
 
 let inputs = document.querySelectorAll(".score");
